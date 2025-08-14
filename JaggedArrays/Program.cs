45c64415549662a7a5ea992a95941bb96ee5ed5c
Joao Vitor Girardi Penso <456113@unoesc.lan>
2025-08-13 21:43:46 -0300

    JaggedArrays

---
+++ JaggedArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿// JaggedArrays
+//sao arrays de arrays
+//ou arrays irregulares
+int[][] numbers = new int[4][];
+numbers[0] = new int[] { 1, 2, 3, };
+numbers[1] = new int[] { 4 };
+numbers[2] = new int[] { 5, 6, 7, 8, 9 };
+numbers[3] = new int[] { 6, 5, 4 };
+
+//outra forma de incializar
+int[][] numbers2 =
+{
+    new int[] { 1,},
+new int[] { 2 },
+new int[] { 3 },
+new int[] { 4 },
+};
+
+//para percorrer o JegueArray utilizamos laços de repetição encadeados / nested loop
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write("[");
+    for (int j = 0; j < numbers[i].Length; j++)
+    {
+        Console.Write($" {numbers[i][j]}");
+
+    }
+    Console.Write("]");
+    Console.WriteLine("");
+}
